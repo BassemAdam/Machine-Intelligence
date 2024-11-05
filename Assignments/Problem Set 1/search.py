f49e35efcfcafb025475fec89b5bf8183f31a381
Bassem Sherief <108496148+BassemAdam@users.noreply.github.com>
2024-11-05 23:22:20 +0200

    Add more comments to explain other searching algorithms

--- Assignments/Problem Set 1/search.py
+++ Assignments/Problem Set 1/search.py
@@ -147,20 +147,21 @@ def AStarSearch(problem: Problem[S, A], initial_state: S, heuristic: HeuristicFu
     frontier.put((initial_f_value, sequence, initial_g_value, initial_state, None, None))
     
     # Track explored states and their cumulative g-values
-    explored = {initial_state: initial_g_value}  # state -> minimum g_value encountered
+    explored = {initial_state: initial_g_value}  # state -> minimum g_value encountered you will find that below in the code we update the g_value if we found a better path to the state
     parent = {}  # state -> (parent_state, action)
     
-    while not frontier.empty():
+    while not frontier.empty(): # while the frontier is not empty we will keep searching and expanding the states in the above searches i didnt write this comment 
         # Get state with the lowest f-value
         f_value, _, g_value, current_state, parent_state, action = frontier.get()
         
-        # If we already have a lower g-value for this state, skip it
+        # If we already have a lower g-value for this state, skip it to make sure that we are not going to expand the state again if we found a better path to it
         if current_state in explored and explored[current_state] < g_value:
             continue
 
-        # Store parent information for path reconstruction if state is not the initial state
+        # Store parent information for path reconstruction if state is not the initial state aka we store only nodes with parents (inital state has no parent)
         if parent_state is not None:
             parent[current_state] = (parent_state, action)
+            # so if iam the current state i will store my parent and the action that led me to this state to know how to return exactly like i came here
             # Point(1, 1): (Point(1, 0), "DOWN"), The current position/state 
             # parent_state: The previous position/state that led here
             # action: The move that got us from parent to current state
@@ -168,25 +169,25 @@ def AStarSearch(problem: Problem[S, A], initial_state: S, heuristic: HeuristicFu
 
         
         # Check if goal is reached
-        # notice that iam here only stop when we dequeue the goal state not stop when we enqueue it
+        # notice that iam here only stop when we dequeue the goal state not stop when we enqueue it why ?? because if we checked goal in the enqueue step we will not be sure that this is the best path to the goal maybe there will be better path
         if problem.is_goal(current_state):
             # Reconstruct path from goal to initial state
             path = []
-            while parent_state is not None:
+            while parent_state is not None: # we keep going up and saving the actions that led us to the goal state
                 path.append(action)
                 current_state = parent_state
                 parent_state, action = parent.get(current_state, (None, None))
             return list(reversed(path))
         
-        # Expand current state
+        # Expand current state after we checked that the current state is not the goal state with all possible actions
         for next_action in problem.get_actions(current_state):
             next_state = problem.get_successor(current_state, next_action)
-            next_g = g_value + problem.get_cost(current_state, next_action)  # Cumulative g-cost
-            next_f = next_g + heuristic(problem, next_state)
+            next_g = g_value + problem.get_cost(current_state, next_action)  # Cumulative g-cost = g-value is the cumulative cost of last actions + cost of new action
+            next_f = next_g + heuristic(problem, next_state) # we prioritize the states with the lowest f-value which is the sum of the g-value and the heuristic value of the state
             
-            # Only add next_state to frontier if it has a lower g-value than previously found
+            # Only add next_state to frontier if it has a lower g-value than previously found or it is unexplored
             if next_state not in explored or next_g < explored[next_state]:
-                sequence += 1
+                sequence += 1 # increment the sequence to break the tie if we have two states with the same f-value
                 frontier.put((next_f, sequence, next_g, next_state, current_state, next_action))
                 explored[next_state] = next_g  # Update to the new lowest g-value
 
@@ -195,13 +196,14 @@ def AStarSearch(problem: Problem[S, A], initial_state: S, heuristic: HeuristicFu
 
 def BestFirstSearch(problem: Problem[S, A], initial_state: S, heuristic: HeuristicFunction) -> Solution:
     #TODO: ADD YOUR CODE HERE
-     # Initialize frontier with (h_value, sequence, state, parent, action)
+    # or its called Greedy Best First Search its the same as A* but it doesnt consider the cost of the path only the heuristic value its the rabbit 
+    # Initialize frontier with (h_value, sequence, state, parent, action)
     frontier = PriorityQueue()
     sequence = 0
     frontier.put((heuristic(problem, initial_state), sequence, initial_state, None, None))
     
     # Track explored states
-    explored = {initial_state}
+    explored = {initial_state} # we only need to track the explored states because we dont consider the cost of the path not like above
     parent = {}  # state -> (parent_state, action)
     
     while not frontier.empty():
